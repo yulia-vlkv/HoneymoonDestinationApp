a911460824813ae28f37952f7c445879e11c0896
Iuliia Vlkv <yu.wlkw@gmail.com>
2023-07-16 10:28:23 +0500

    Initial Commit

---
+++ HoneymoonDestination/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HoneymoonDestination
+//
+//  Created by Iuliia Volkova on 16.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HoneymoonDestination/HoneymoonDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonDestinationApp.swift
+//  HoneymoonDestination
+//
+//  Created by Iuliia Volkova on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
