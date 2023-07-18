b11645a3aa44b73f5b06c33a5c4474c9d819c894
Iuliia Vlkv <yu.wlkw@gmail.com>
2023-07-18 19:09:13 +0500

    Added HeaderView

---
+++ HoneymoonDestination/View/HeaderView.swift
@@ -0,0 +1,51 @@
+//
+//  HeaderView.swift
+//  HoneymoonDestination
+//
+//  Created by Iuliia Volkova on 18.07.2023.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                //Action
+                print("Information")
+            }) {
+                Image(systemName: "info.circle")
+                    .font(.system(size: 24, weight: .regular))
+            }
+            .accentColor(Color.primary)
+            
+            Spacer()
+            
+            Image("logo-honeymoon-pink")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 28)
+            
+            Spacer()
+            
+            Button(action: {
+                //Action
+                print("Guide")
+            }) {
+                Image(systemName: "questionmark.circle")
+                    .font(.system(size: 24, weight: .regular))
+            }
+            .accentColor(Color.primary)
+        }
+        .padding()
+    }
+}
+
+
+// MARK: - Preview
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.fixed(width: 375, height: 80))
+    }
+}
