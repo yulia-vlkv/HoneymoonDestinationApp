42e071b658672f9342531c2306cf93f8904d45ec
Iuliia Vlkv <yu.wlkw@gmail.com>
2023-07-19 19:28:46 +0500

    Added a guide view

---
+++ HoneymoonDestination/View/GuideView.swift
@@ -0,0 +1,76 @@
+//
+//  GuideView.swift
+//  HoneymoonDestination
+//
+//  Created by Iuliia Volkova on 19.07.2023.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started!")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.pink)
+                
+                Text("Discover and pick the perfect destination \nfor your romantic Honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(
+                        title: "Like",
+                        subtitle: "Swipe right",
+                        description: "Do you like this destination? Touch the screen and swipe right. It will be saved to the favourites.",
+                        icon: "heart.circle")
+                    
+                    GuideComponent(
+                        title: "Dismiss",
+                        subtitle: "Swipe left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. You will no longer see it.",
+                        icon: "xmark.circle")
+                    
+                    GuideComponent(
+                        title: "Book",
+                        subtitle: "Tap the button",
+                        description: "Our selection of honeymoon resorts is perfect setting for you to embark your new life together.",
+                        icon: "checkmark.square")
+                }
+                
+                Spacer(minLength: 10)
+                
+                Button(action: {
+                    // ACTION
+                    print("A button was tapped")
+                }) {
+                    Text("Continie".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Capsule().fill(Color.pink))
+                        .foregroundColor(Color.white)
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 13)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        }
+    }
+}
+    
+    // MARK: - Preview
+    struct GuideView_Previews: PreviewProvider {
+        static var previews: some View {
+            GuideView()
+        }
+    }
