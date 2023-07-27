dbf8eed90ed8526a49ed9fe5e3cfde9fd5a2a89a
Iuliia Vlkv <yu.wlkw@gmail.com>
2023-07-27 18:58:06 +0500

    Added 2 CardViews to ContentView

--- HoneymoonDestination/View/ContentView.swift
+++ HoneymoonDestination/View/ContentView.swift
@@ -14,19 +14,48 @@ struct ContentView: View {
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
     
+    // MARK: - Card Views
+    
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        for index in 0..<2 {
+            views.append(CardView(honeymoon: honeymoonData[index]))
+        }
+        return views
+    }()
+    
+    
+    // MARK: - Top Card
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: {$0.id == cardView.id}) else {
+            return false
+        }
+        return index == 0
+    }
+    
     // MARK: - Body
     var body: some View {
         VStack {
+            
+            // MARK: - Header
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
             
             Spacer()
             
-            CardView(honeymoon: honeymoonData[2])
+            // MARK: - Cards
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            .padding(.horizontal)
             
             //Fixme: Add padding to the cards
             
             Spacer()
             
+            // MARK: - Footer
             FooterView(showBookingAlert: $showAlert)
         }
         .alert(isPresented: $showAlert) {
