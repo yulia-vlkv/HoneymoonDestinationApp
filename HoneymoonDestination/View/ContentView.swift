9d56b8880cc168dded308b13b17861475f602aed
Iuliia Vlkv <yu.wlkw@gmail.com>
2023-08-04 18:03:01 +0500

    Added symbols to appear during swiping

--- HoneymoonDestination/View/ContentView.swift
+++ HoneymoonDestination/View/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
     @GestureState private var dragState = DragState.inactive
+    private var dragAreaThreshold: CGFloat = 65.0
     
     // MARK: - Card Views
     
@@ -85,7 +86,20 @@ struct ContentView: View {
                 ForEach(cardViews) { cardView in
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
-                        .offset(x: self.isTopCard(cardView: cardView) ? self.dragState.translation.width : 0, y: self.isTopCard(cardView: cardView) ? self.dragState.translation.height : 0)
+                        .overlay(
+                            ZStack {
+                                // X-MARK SYMBOL
+                                Image(systemName: "x.circle")
+                                    .modifier(SymbolModifier())
+                                    .opacity(self.dragState.translation.width < -self.dragAreaThreshold && self.isTopCard(cardView: cardView) ? 1 : 0.0)
+                                
+                                // HEART SYMBOL
+                                Image(systemName: "heart.circle")
+                                    .modifier(SymbolModifier())
+                                    .opacity(self.dragState.translation.width > self.dragAreaThreshold && self.isTopCard(cardView: cardView) ? 1 : 0.0)
+                            }
+                        )
+                        .offset(x: self.isTopCard(cardView: cardView) ? self.dragState.translation.width : 0)
                         .scaleEffect(self.dragState.isDragging && self.isTopCard(cardView: cardView) ? 0.88 : 1.0)
                         .rotationEffect(Angle(degrees: self.isTopCard(cardView: cardView) ? Double(self.dragState.translation.width / 12) : 0))
                         .animation(.interpolatingSpring(stiffness: 120, damping: 120), value: dragState.isDragging ? true : false)
